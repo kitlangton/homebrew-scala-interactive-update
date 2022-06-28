ed639cf13b2e0725f32234e3a06cae5ab5d4fa40
Kit Langton <kit.langton@gmail.com>
2022-06-28 09:39:39 -0400

    scala-interactive-update 0.1.10

--- Formula/scala-interactive-update.rb
+++ Formula/scala-interactive-update.rb
@@ -1,8 +1,8 @@
 class ScalaInteractiveUpdate < Formula
   desc "Update your Scala dependencies interactively"
   homepage "https://github.com/kitlangton/scala-interactive-update"
-  url "https://github.com/kitlangton/scala-interactive-update/releases/download/v0.1.9/sup-macos"
-  sha256 "d177b195f2af885e277d183e6c6d83a626145fd12138dbae188473efd3adcfe0"
+  url "https://github.com/kitlangton/scala-interactive-update/releases/download/v0.1.10/sup-macos-amd-"
+  sha256 "aee04fa3aa8ebeede4cd9db70671dfb52ab6e26c2f8c30d11d91309532cd5b63"
 
   def install
     bin.install "sup-macos" => "scala-interactive-update"
