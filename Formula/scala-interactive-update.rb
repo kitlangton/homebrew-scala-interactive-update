5b7f0b5d46cba1781e875ff36ca73cc88e7108ba
Kit Langton <kit.langton@gmail.com>
2022-06-27 20:28:16 -0400

    update installation

--- Formula/scala-interactive-update.rb
+++ Formula/scala-interactive-update.rb
@@ -4,9 +4,8 @@ class ScalaInteractiveUpdate < Formula
   url "https://github.com/kitlangton/scala-interactive-update/releases/download/v0.1.3/scala-interactive-update.zip"
   sha256 "8153d48aa819eada3eecaf47560aa68eed782a61f9c65564db4ee9ccd3aeba3a"
 
-
   def install
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    bin.install "scala-interactive-update"
   end
 
 end
