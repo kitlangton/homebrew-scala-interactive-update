7ec25c67881a829f546d52d444e7c9ffe4dba913
Kit Langton <kit.langton@gmail.com>
2022-06-28 00:10:06 -0400

    Update scala-interactive-update.rb

--- Formula/scala-interactive-update.rb
+++ Formula/scala-interactive-update.rb
@@ -1,7 +1,7 @@
 class ScalaInteractiveUpdate < Formula
   desc "Update your Scala dependencies interactively"
   homepage "https://github.com/kitlangton/scala-interactive-update"
-  url "https://github.com/kitlangton/scala-interactive-update/releases/download/v0.1.8/sup-macos-amd64"
+  url "https://github.com/kitlangton/scala-interactive-update/releases/download/v0.1.8/sup-macos"
   sha256 "00be5dc93c410122bf88d39bc1d9f93be66c67638bf1718402d5c83d45b19ec9"
 
   def install
