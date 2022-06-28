f6a11972cedd7dfc905622397d305122265e5a97
runner <runner@Mac-1656423702196.local>
2022-06-28 14:14:09 +0000

    scala-interactive-update 0.1.12

--- Formula/scala-interactive-update.rb
+++ Formula/scala-interactive-update.rb
@@ -1,8 +1,8 @@
 class ScalaInteractiveUpdate < Formula
   desc "Update your Scala dependencies interactively"
   homepage "https://github.com/kitlangton/scala-interactive-update"
-  url "https://github.com/kitlangton/scala-interactive-update/releases/download/v0.1.9/sup-macos"
-  sha256 "d177b195f2af885e277d183e6c6d83a626145fd12138dbae188473efd3adcfe0"
+  url "https://github.com/kitlangton/scala-interactive-update/releases/download/v0.1.12/sup-macos-amd"
+  sha256 "c41a972917f921ac8751b7e80c88be3a3487f5d6f0838272fb8bef9e3982d27c"
 
   def install
     bin.install "sup-macos" => "scala-interactive-update"
