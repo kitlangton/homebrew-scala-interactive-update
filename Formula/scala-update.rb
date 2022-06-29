0cfbd1c51612eb07596fa32a84159758a5a3ba34
Kit Langton <kit.langton@gmail.com>
2022-06-29 14:19:21 -0400

    Update and rename scala-interactive-update.rb to scala-update.rb

--- Formula/scala-update.rb
+++ Formula/scala-update.rb
@@ -1,10 +1,10 @@
-class ScalaInteractiveUpdate < Formula
+class ScalaUpdate < Formula
   desc "Update your Scala dependencies interactively"
-  homepage "https://github.com/kitlangton/scala-interactive-update"
+  homepage "https://github.com/kitlangton/scala-update"
   url "https://github.com/kitlangton/scala-interactive-update/releases/download/v0.1.15/sup-macos-amd"
   sha256 "282d01c21eca7c3d9a9d725bcb37f4045cefabb1f78e7ada1ac95e75bc1a37fd"
 
   def install
-    bin.install "sup-macos-amd" => "scala-interactive-update"
+    bin.install "sup-macos-amd" => "scala-update"
   end
 end
