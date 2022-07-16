9bb845de7843dade39fc3c3e6a8b172c3ce51129
runner <runner@Mac-1657982226019.local>
2022-07-16 15:07:05 +0000

    scala-update 0.2.2

--- Formula/scala-update.rb
+++ Formula/scala-update.rb
@@ -1,8 +1,8 @@
 class ScalaUpdate < Formula
   desc "Update your Scala dependencies interactively"
   homepage "https://github.com/kitlangton/scala-update"
-  url "https://github.com/kitlangton/scala-update/releases/download/v0.2.1/sup-macos-amd"
-  sha256 "e24c8e11a6461b7a95635ac2f87a9ba8b89582c95dfbeaa4e8ee424edf7bf0b4"
+  url "https://github.com/kitlangton/scala-update/releases/download/v0.2.2/sup-macos-amd"
+  sha256 "3918f28f30217d57fb81b2e2ad1cccfa8a5d5fe0e11733ba98fdc20112b6ee57"
 
   def install
     bin.install "sup-macos-amd" => "scala-update"
