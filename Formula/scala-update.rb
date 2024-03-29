a3b1d510c79f8daa7457142454f6d5daaec67c9e
Kit Langton <kit.langton@gmail.com>
2024-03-29 09:50:14 -0400

    fix

--- Formula/scala-update.rb
+++ Formula/scala-update.rb
@@ -1,8 +1,8 @@
 class ScalaUpdate < Formula
   desc "Update your Scala dependencies interactively"
   homepage "https://github.com/kitlangton/scala-update"
-  url "https://github.com/kitlangton/scala-update/releases/download/v0.3.0/sup-macos-amd"
-  sha256 "3bd3d43ec41546ccf7f8eefcad8b5815a94457f3963c5263a371a6d52ea18f88"
+  url "https://github.com/kitlangton/scala-update/releases/download/v0.2.1/sup-macos-amd"
+  sha256 "e24c8e11a6461b7a95635ac2f87a9ba8b89582c95dfbeaa4e8ee424edf7bf0b4"
 
   def install
     bin.install "sup-macos-amd" => "scala-update"
