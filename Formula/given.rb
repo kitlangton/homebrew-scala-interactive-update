7dd9d002059b54c6cff9f7c5be9eb9776c102d1a
Kit Langton <kit.langton@gmail.com>
2024-06-06 07:22:17 -0400

    update given formula

--- Formula/given.rb
+++ Formula/given.rb
@@ -1,10 +1,24 @@
 class Given < Formula
   desc "Interactively update your Scala dependencies"
   homepage "https://github.com/kitlangton/given"
-  url "https://github.com/kitlangton/given/releases/download/v0.1.6/given_macos_arm"
-  sha256 "72ee29ece71f6333306300ba86cefe7992b3fd88790c7f818e7713f824d0423e"
+  url "https://github.com/kitlangton/given/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "f2068ccefe830cfdfbe978a870b39f6ecb0897572233965aed0c0d44192fa8db"
+  head "https://github.com/kitlangton/given.git"
+
+  depends_on "rust" => :build
 
   def install
-    bin.install "given_macos_arm" => "given"
+    system "cargo", "install", *std_cargo_args
+  end
+
+  def caveats
+    <<~EOS
+      Thank you for installing \e[32mΔ GIVEN\e[0m!
+
+      To get started run `\e[32mgiven\e[0m` in a Scala repo.
+
+      For more information, visit:
+        \e[34mhttps://github.com/kitlangton/given\e[0m
+    EOS
   end
 end
